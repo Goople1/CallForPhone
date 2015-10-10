09c6b8c4c2956e6606288111a204453b485f7470
Goople1 <jsoteloj@upao.edu.pe>
2015-10-10 15:58:33 -0500

    Url

--- jkProducto/jkProducto/urls.py
+++ jkProducto/jkProducto/urls.py
@@ -9,6 +9,18 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'productos.views.home', name='home'),
     url(r'^login/$', 'productos.views.iniciarSesion', name='login'),
-    url(r'^sucursales/$', 'sucursales.views.listarSucusarles', name='sucursales'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('sucursales.views',
+ 	url(r'^sucursales/$', 'listarSucusarles', name='sucursales'),
+ 	url(r'^mantenimientoSucursal/$', 'mantenimientoSucursal', name='mantenimientoSucursal'),
+ 	url(r'^mantenimientoSucursal/add/$', 'addSucursal', name='add'),
+ 	url(r'^mantenimientoSucursal/edit/$', 'editSucursal', name='edit'),
+ 	url(r'^mantenimientoSucursal/list/$', 'listSucursal', name='list'),
+ 	url(r'^mantenimientoSucursal/add/(?P<slug>[\w-]+)/$', 'addSucursal',name = 'adds'),
+ 	url(r'^mantenimientoSucursal/edit/(?P<slug>[\w-]+)/$', 'editSucursal'),
+ 	url(r'^mantenimientoSucursal/list/(?P<slug>[\w-]+)/$', 'listSucursal'),
+ 	
+
+ )
--- jkProducto/sucursales/views.py
+++ jkProducto/sucursales/views.py
@@ -2,7 +2,34 @@ from django.shortcuts import render,render_to_response
 from django.template.context import RequestContext
 from models import Sucursal
 # Create your views here.
+
+def mantenimientoSucursal(request):
+	template = 'MantenimientoAsignacionSucursales.html'
+	return render_to_response(template,{},context_instance=RequestContext(request))
+
+def addSucursal(request):
+	template='ListarSucursales.html'
+	operation = 'add'
+	sucursal = Sucursal.objects.all()
+	return render_to_response(template,{'sucursales':sucursal,'operation':operation},context_instance=RequestContext(request))
+
+def editSucursal(request):
+	template='ListarSucursales.html'
+	operation = 'edit'
+	sucursal = Sucursal.objects.all()
+	return render_to_response(template,{'sucursales':sucursal,'operation':operation},context_instance=RequestContext(request))
+
+def listSucursal(request):
+	template='ListarSucursales.html'
+	operation = 'list'
+	sucursal = Sucursal.objects.all()
+	return render_to_response(template,{'sucursales':sucursal,'operation':operation},context_instance=RequestContext(request))
+
 def listarSucusarles(request):
 	template='ListarSucursales.html'
 	sucursal = Sucursal.objects.all()
 	return render_to_response(template,{'sucursales':sucursal},context_instance=RequestContext(request))
+
+#Asignacion de Pedidos a Sucursales
+def registrarPedidoSucursal(request):
+	pass
